d096b74a850050736dd467c795d83f65eacb068c
Roksana Islam <roksana.islam@northsouth.edu>
2020-11-02 06:21:08 +0600

    first lab task

---
+++ Assignments/Lab/Lab 01 - Oct 29 2020 Thursday/HelloWorld/app/src/main/java/com/northsouth/ece/fall2020/cse468/sec1/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.northsouth.ece.fall2020.cse468.sec1.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e("MainActivity onCreate", "Hello World");
+    }
+}
