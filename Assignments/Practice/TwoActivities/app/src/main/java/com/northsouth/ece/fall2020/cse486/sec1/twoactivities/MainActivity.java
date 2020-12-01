7fc3008ed5c02080cdae791408335ba6c672c0e5
Roksana Islam <roksana.islam@northsouth.edu>
2020-12-01 23:37:18 +0600

    Two Activties practice

---
+++ Assignments/Practice/TwoActivities/app/src/main/java/com/northsouth/ece/fall2020/cse486/sec1/twoactivities/MainActivity.java
@@ -0,0 +1,32 @@
+package com.northsouth.ece.fall2020.cse486.sec1.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.northsouth.ece.fall2020.cse486.sec1.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        String message = mMessageEditText.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
